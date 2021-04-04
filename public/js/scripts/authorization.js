fc29674688ef430fab29286bb0766ae7f023e3ea
Vlad Cherokov <vcherokov@mail.ru>
2021-04-04 03:20:57 +0300

    Отредактировал внешний вид форм авторизации/регистрации

--- public/js/scripts/authorization.js
+++ public/js/scripts/authorization.js
@@ -1,19 +1,30 @@
 
 $(document).ready(function() {
-    $("#authorization_button").on("click", function(){
-        $("#authorization_modal").css("display", "flex"); 
-    });
-    $("#registration_button").on("click", function(){
-        $("#registration_modal").css("display", "flex"); 
+
+    // Нажатие на кнопку "Авторизоваться"
+    $("#authorization_button, .modals button:eq(0)").on("click", function(){
+        $("#authorization_modal").css("display", "flex");
+        $("#form_registration").css("display", "none");
+        $("#form_authorization").css("display", "block");
+        $(".modals button:eq(0)").css("background-color", "#5B5B5B");
+        $(".modals button:eq(1)").css("background-color", "#A7A7A7");
     });
 
-    $(".close_modal").on("click", function(){
-        $(".modals_wrapper").css("display", "none");
+    // Нажатие на кнопку "Зарегистрироваться"
+    $("#registration_button, .modals button:eq(1)").on("click", function(){
+        $("#authorization_modal").css("display", "flex"); 
+        $("#form_registration").css("display", "block");
+        $("#form_authorization").css("display", "none");
+        $(".modals button:eq(1)").css("background-color", "#5B5B5B");
+        $(".modals button:eq(0)").css("background-color", "#A7A7A7");
+    });
+
+    // Обработчик выхода из модального окна
+    $("body").on("click", function(e){
+        var el = e ? e.target : window.event.srcElement;
+        if (el.className === 'modals_wrapper') $(".modals_wrapper").css("display", "none");
     });
     /*
-    $("body").on("click", ".background_wrapper", function(){
-        alert(1);
-    });
     $("body").on("click", ".authorization_form", function(){
         alert(2);
     });
