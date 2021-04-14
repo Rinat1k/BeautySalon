926571ef688a0dcd361dd57a0a641f9d3d6c0628
Rinat <roma.tankin@gmail.com>
2021-04-14 11:25:10 +0300

    исправлен вывод каталога

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,10 @@ const bodyParser = require("body-parser");
 const registerMidleware = require("../midleware/register");
 const loginMidleware = require("../midleware/login");
 const catalogMidleware = require("../midleware/catalog");
+const indexMidleware = require("../midleware/index");
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
-router.get("/",(req,res)=>
-{
-    res.render("index",{
-        title:"Главная страница"
-    });
-});
+router.get("/",indexMidleware);
 router.get("/catalog",catalogMidleware);
 
 router.get("/map",(req,res)=>
